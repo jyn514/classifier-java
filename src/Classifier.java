aa0bc8e67ae5f20481f377dc457f5569830e5c51
Joshua Nelson <jyn514@gmail.com>
2017-10-08 16:52:17 -0400

    Made getOptions a static method.

--- src/Classifier.java
+++ src/Classifier.java
@@ -1,9 +1,15 @@
 package src;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 /**
  * @author Joshua Nelson
  * @email jyn514@gmail.com
- * @version 0.1 (2017-9-17)
+ * @version 0.1 (2017-10-11)
+ *
  * Classifier; a simple program to organize a directory
  * 
  * Copyright (C) 2017  Joshua Nelson
@@ -27,20 +33,126 @@ package src;
 
 class Classifier {
 
-	static final String VERSION = "0.1";
+  public static final String VERSION = "0.1";
 
-	static Parameters parsedArgs = ReadOnly.parameters; 
+  static Parameters parsedArgs = ReadOnly.parameters; // not actually parsed until ReadOnly.instance.parse(args) is
+                                                     // executed
 
-	public static void main(String[] args) {
-		ReadOnly.instance.parse(args);
-		new getOptions(parsedArgs);
-		
-		if (parsedArgs.exitImmediately) {
-			return;
-		}
+  private static final Path MASTER = ReadOnly.MASTERCONF; // got tired of typing
 
-		ReadOnly.classifyByExtension(parsedArgs.parsedConfig, parsedArgs.path);
+  private static final String INVALID_PATH = "Invalid path or cannot write to file. Type -h for help.";
 
-	}
+  private static Path LOCAL; // shouldn't really be static
 
+  public static void main(String[] args) {
+    ReadOnly.instance.parse(args);
+
+    if (ReadOnly.Validator.pathIsValid(parsedArgs.directory)) {
+      parsedArgs.path = ReadOnly.Validator.path;
+    } else {
+      System.out.println(INVALID_PATH);
+      return;
+    }
+
+    getOptions(parsedArgs);
+
+    if (parsedArgs.exitImmediately) {
+      return;
+    }
+
+    ReadOnly.classifyByExtension(parsedArgs.parsedConfig, parsedArgs.path);
+
+  }
+
+  static void getOptions(Parameters parameters) {
+
+    if ((parameters.resetLocal || parameters.editLocal) && parameters.directory.isEmpty()) {
+      System.out.println("Please specify a directory.");
+      parameters.exitImmediately = true;
+
+    } else if (parameters.version || parameters.showDefault || parameters.config || parameters.editLocal
+        || parameters.edit || parameters.reset || parameters.resetLocal || parameters.help || parameters.copyright) {
+
+      parameters.exitImmediately = true;
+      LOCAL = getLocal(parameters);
+      iterateArgs(parameters, new Scanner(System.in));
+
+    } else if (parameters.directory.isEmpty()) {
+      // checks for null string, not if dir is empty
+      System.out.println(ReadOnly.HELP);
+      parameters.exitImmediately = true;
+    } else {
+      try {
+        parameters.parsedConfig = ReadOnly.parseConfigList(ReadOnly.getConf(parameters.path));
+      } catch (IOException e) {
+        System.out.println("Could not read config file");
+        ReadOnly.checkStackTraceOnError(e);
+      }
+
+    }
+
+  }
+
+  private static void iterateArgs(Parameters parameters, Scanner scanner) {
+    if (parameters.version) {
+      System.out.println(Classifier.VERSION);
+    } else if (parameters.help) {
+      ReadOnly.instance.usage();
+    } else if (parameters.showDefault) {
+      System.out.print(ReadOnly.listToString(ReadOnly.DEFAULT));
+    } else if (parameters.copyright) {
+      System.out.println(ReadOnly.COPYRIGHT);
+    } else if (parameters.reset) {
+      reset(MASTER, scanner);
+    } else if (parameters.resetLocal) {
+      if (parameters.directory.isEmpty()) {
+        System.out.println("Please specify a directory.");
+        return;
+      }
+      reset(LOCAL, scanner);
+    } else if (parameters.config) {
+      try {
+        System.out.print(ReadOnly.listToString(ReadOnly.getConf(parameters.path)));
+      } catch (IOException e) {
+        System.out.println("Could not access one or more files.");
+        ReadOnly.checkStackTraceOnError(e);
+      }
+    } else if (parameters.edit) {
+      try {
+        ReadOnly.edit(MASTER);
+      } catch (IOException e) {
+        System.out.println("Could not access " + MASTER);
+        ReadOnly.checkStackTraceOnError(e);
+      }
+    } else if (parameters.editLocal) {
+      try {
+        ReadOnly.edit(LOCAL);
+      } catch (IOException e) {
+        System.out.println("Could not access " + LOCAL);
+        ReadOnly.checkStackTraceOnError(e);
+      }
+    }
+  }
+
+  private static Path getLocal(Parameters parameters) throws IllegalArgumentException {
+    return parameters.path.resolve(".classifier.conf").toAbsolutePath().normalize();
+  }
+
+  private static void reset(Path p, Scanner scanner) {
+    if (!Files.exists(p)) {
+      return;
+    }
+    System.out.print("Are you sure you want to delete " + p + " ? [y/n] ");
+    String stdin = scanner.next().toLowerCase();
+    if (stdin.equals("yes") || stdin.equals("y")) {
+      try {
+        Files.delete(p);
+      } catch (IOException e) {
+        System.out.print("Could not delete " + p + ", make sure you have the proper permissions.");
+        ReadOnly.checkStackTraceOnError(e);
+      }
+    } else {
+      System.out.println("Aborting.");
+    }
+  }
 }
